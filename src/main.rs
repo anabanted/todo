0907f785a87fa26ba7146c4c92de0336b0bfe91d
Shun Ono <onoshun26@gmail.com>
2022-12-03 00:23:47 +0900

    done chapt 5-4 but it crashes when adding todos

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, http::header, post, web, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
+use serde::Deserialize;
 use thiserror::Error;
+
+#[derive(Deserialize)]
+struct AddParams {
+    text: String,
+}
+
+#[derive(Deserialize)]
+struct DeleteParams {
+    id: u32,
+}
 
 struct TodoEntry {
     id: u32,
@@ -30,27 +41,28 @@ enum MyError {
 
 impl ResponseError for MyError {}
 
-#[actix_rt::main]
-async fn main() -> Result<(), actix_web::Error> {
-    let manager = SqliteConnectionManager::file("todo.db");
-    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
-    let conn = pool
-        .get()
-        .expect("Failed to get the connection from the pool.");
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS todo (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            text TEXT NOT NULL
-        )",
-        params![],
-    )
-    .expect("Failed to create a table `todo`.");
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .append_header((header::LOCATION, "/"))
+        .finish())
+}
 
-    HttpServer::new(move || App::new().service(index).data(pool.clone()))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await?;
-    Ok(())
+#[post("/delete")]
+async fn delete_todo(
+    params: web::Form<DeleteParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("DELETE FROM todo WHERE id=?", &[&params.id])?;
+    Ok(HttpResponse::SeeOther()
+        .append_header((header::LOCATION, "/"))
+        .finish())
 }
 
 #[get("/")]
@@ -72,4 +84,33 @@ async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpRespo
     Ok(HttpResponse::Ok()
         .content_type("text/html")
         .body(response_body))
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), actix_web::Error> {
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
+    let conn = pool
+        .get()
+        .expect("Failed to get the connection from the pool.");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todo (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        )",
+        params![],
+    )
+    .expect("Failed to create a table `todo`.");
+
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(add_todo)
+            .service(delete_todo)
+            .data(pool.clone())
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await?;
+    Ok(())
 }
